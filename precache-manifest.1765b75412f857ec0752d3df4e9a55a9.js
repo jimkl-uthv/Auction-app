82d4e0c6633976b3704024f3d2b806dad0a8415c
dkladouchos <dkladouchos@uth.gr>
2021-05-24 19:47:03 +0300

    Updates

---
+++ precache-manifest.1765b75412f857ec0752d3df4e9a55a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d875d20f0f772ac451636406405ede",
+    "url": "/Auction-app/index.html"
+  },
+  {
+    "revision": "5ba8649887df019362fc",
+    "url": "/Auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d06a9ac865cc224e53ef",
+    "url": "/Auction-app/static/js/2.da5afb8b.chunk.js"
+  },
+  {
+    "revision": "5ba8649887df019362fc",
+    "url": "/Auction-app/static/js/main.c6a868fc.chunk.js"
+  },
+  {
+    "revision": "f7472452af8151a2b3cc",
+    "url": "/Auction-app/static/js/runtime-main.0409cdc2.js"
+  }
+]);
